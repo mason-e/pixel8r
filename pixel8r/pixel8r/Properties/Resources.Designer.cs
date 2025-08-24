df687ca7a9b3f38d54568952698efe2205bb1918
mason-e <engelmason@gmail.com>
2025-08-24 12:52:32 -0600

    Rename project and folders

--- pixel8r/pixel8r/Properties/Resources.Designer.cs
+++ pixel8r/pixel8r/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CSharpGenerator.Properties {
+namespace pixel8r.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CSharpGenerator.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CSharpGenerator.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("pixel8r.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
