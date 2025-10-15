737e6892f497686bcf9e344b4a3b2b58caa93695
mason-e <engelmason@gmail.com>
2025-10-15 17:57:16 -0600

    Binding to anticipated changing variables

--- pixel8r-avalonia/pixel8r_avalonia/ViewModels/MainViewModel.cs
+++ pixel8r-avalonia/pixel8r_avalonia/ViewModels/MainViewModel.cs
@@ -1,7 +1,46 @@
-﻿
+﻿using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
+using ReactiveUI;
+
 namespace pixel8r_avalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private string _filePath = "No file loaded. Select a file with the open button to enable more editing controls.";
+    public string FilePath
+    {
+        get => _filePath;
+        set => this.RaiseAndSetIfChanged(ref _filePath, value);
+    }
+
+    private string? _pendingEdit;
+    public string? PendingEdit
+    {
+        get => _pendingEdit;
+        set => this.RaiseAndSetIfChanged(ref _pendingEdit, value);
+    }
+
+    private string? _imageDimensions;
+    public string? ImageDimensions
+    {
+        get => _imageDimensions;
+        set => this.RaiseAndSetIfChanged(ref _imageDimensions, value);
+    }
+
+    private int _imageWidth = 1280;
+    public int ImageWidth
+    {
+        get => _imageWidth;
+        set => this.RaiseAndSetIfChanged(ref _imageWidth, value);
+    }
+
+    private int _imageHeight = 720;
+    public int ImageHeight
+    {
+        get => _imageHeight;
+        set => this.RaiseAndSetIfChanged(ref _imageHeight, value); 
+    }
 }
