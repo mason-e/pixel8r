888b052b5fd130bfdc9f898b870153996bc9377b
mason-e <engelmason@gmail.com>
2025-10-21 19:21:10 -0600

    Open file and set path

--- pixel8r-avalonia/pixel8r_avalonia/ViewModels/MainViewModel.cs
+++ pixel8r-avalonia/pixel8r_avalonia/ViewModels/MainViewModel.cs
@@ -1,14 +1,27 @@
-﻿using System.Linq;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform.Storage;
+﻿using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using ReactiveUI;
 
 namespace pixel8r_avalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
+    private readonly Interaction<string?, string?> _OpenFileInteraction;
+    public Interaction<string?, string?> OpenFileInteraction => this._OpenFileInteraction;
+    public MainViewModel()
+    {
+        _OpenFileInteraction = new Interaction<string?, string?>();
+        OpenFileCommand = ReactiveCommand.CreateFromTask(OpenFile);
+    }
+
+    public ICommand OpenFileCommand { get; }
+
+    private async Task OpenFile()
+    {
+        FilePath = await _OpenFileInteraction.Handle(_filePath);
+    }
+
     private string _filePath = "No file loaded. Select a file with the open button to enable more editing controls.";
     public string FilePath
     {
--- pixel8r-avalonia/pixel8r_avalonia/Views/MainView.axaml.cs
+++ pixel8r-avalonia/pixel8r_avalonia/Views/MainView.axaml.cs
@@ -1,11 +1,38 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+using Avalonia.ReactiveUI;
+using pixel8r_avalonia.ViewModels;
+using ReactiveUI;
 
 namespace pixel8r_avalonia.Views;
 
-public partial class MainView : UserControl
+public partial class MainView : ReactiveUserControl<MainViewModel>
 {
     public MainView()
     {
         InitializeComponent();
+
+        this.WhenActivated(d =>
+        {
+            d(ViewModel!.OpenFileInteraction.RegisterHandler(this.OpenFileHandler));
+        });
+    }
+
+    private async Task OpenFileHandler(IInteractionContext<string?, string?> context)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        var storageFiles = await topLevel!.StorageProvider
+            .OpenFilePickerAsync(
+                        new FilePickerOpenOptions()
+                        {
+                            AllowMultiple = false,
+                            Title = "Open Image File",
+                            FileTypeFilter = [FilePickerFileTypes.ImageAll]
+                        });
+
+        context.SetOutput(storageFiles.First().TryGetLocalPath());
     }
 }
